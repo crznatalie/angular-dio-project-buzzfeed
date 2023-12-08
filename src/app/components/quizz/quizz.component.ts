fd05baeb4ad3dec4b074680e62591e8d4e2c94e5
crznatalie <cpnnatalie@gmail.com>
2023-12-08 15:20:15 -0300

    Logic properties creation

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quizz',
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
+
+title:string=""
+
+questions:any
+questionSelected:any
+
+answers:string[] = []
+answerSelected:string =""
+
+questionIndex:number = 0
+questionMaxIndex:number = 0
+
+finished:boolean = false
 
   constructor() { }
 
